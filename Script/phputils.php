b9aa836ede3e1c53d0e0854a8019e3873c4227de
AnatolyGL1966 <k5d7k12@gmail.com>
2015-06-05 15:53:21 +0300

    Version_05_06_2015

--- Script/phputils.php
+++ Script/phputils.php
@@ -16,9 +16,6 @@
         echo('<label  style="width:65%;float:left"> '.$filename.' type '.$fileext.' </label>');
         echo('<label  style="width:10%;float:left"> '.$filesize.' </label>');
         echo('<button style="width:15%;float:left" id="loadVideo'.$ordernum.'" title="Load video button" onclick="getVideobyFileName('.'\''.$filename.'\''.')" >Load Video</button>');
-//        echo('<a      style="width:5%;float:right" href="http://www.ntgl2004.net/images/'.$filename.'" target="_blank" >');
-//        echo('<img    style="width:20px;height:20px" src="Icons/Download.png"  alt="DownLoad" ></img>');
-//        echo('</a>');
         echo('<input type="image" style="width:20px;float:left;height:20px" src="Icons/Download.png" onclick="DownLoadFileByXMLDoc('.'\''.$filename.'\''.')" alt="DownLoad" ></img>');
         echo('</div>');      
   }
@@ -36,6 +33,58 @@
         echo('<img    style="width:15px;height:15px" src="Icons/Download.png"  alt="DownLoad" ></img>');
         echo('</a>');
         echo('</div>');      
+  }
+
+
+  function MakeDivShowImageFile($filename,$fileext,$ordernum,$filesize,$filetime)
+  {
+        echo('<div id="image"'.$ordernum.' style="width:100%">');      
+        echo('<div id="imageVideo"'.$ordernum.' style="display:none;">images/'.$filename.'</div>');
+        echo('<label  style="width:35%;float:left"> '.$filename.' </label>');
+        echo('<label  style="width:10%;float:left"> '.$fileext.' </label>');
+        echo('<label  style="width:10%;float:left"> '.$filesize.' </label>');
+        echo('<label  style="width:25%;float:left"> '.$filetime.' </label>');
+        echo('<a      style="width:5%;float:right" href="View_movie.php?imagefilename='.$filename.'" target="_blank" >');
+        echo('<img    style="width:15px;height:15px" src="Icons/Download.png"  alt="DownLoad" ></img>');
+        echo('</a>');
+        echo('</div>');      
+  }
+
+
+  function ShowImagesFiles()
+  {
+      $dir    = 'images';
+      $files2 = scandir($dir, 0);
+      $lengtharr = count($files2);
+      echo('<header>');
+      if ($lengtharr > 0)
+      {
+       echo('<div id="HeaderImage" style="width:100%">');      
+        echo('<label  style="width:35%;float:left">  File Name  </label>');
+        echo('<label  style="width:10%;float:left">  Ext </label>');
+        echo('<label  style="width:10%;float:left">  Size </label>');
+        echo('<label  style="width:25%;float:left">  Date Time </label>');
+        echo('<label  style="width:20%;float:right">  Action </label>');
+        echo('<br></br>');
+        echo('</div>');      
+     }
+     echo('</header>');
+     echo('<section>');
+     for ($index = 0;$index < $lengtharr;$index++)
+     {
+         $filename =  $files2[$index];
+         if ((trim($filename) != "..") && (trim($filename) != "."))
+         {
+             $fileext = substr($filename,-3);
+             $fileindex= $index+1;
+             $filesize = human_filesize(filesize($dir."/".$filename));
+             $filetime = date("d/m/y H:i:s",filemtime($dir."/".$filename));
+             $filename_sz = $filesize;
+             MakeDivShowImageFile($filename, $fileext, strval($fileindex),$filename_sz,$filetime);
+             echo "<br>";
+        }
+     }  
+     echo('</section>');
   }
 
 function human_filesize($bytes, $decimals = 2)
--- movies_one_page.php
+++ movies_one_page.php
@@ -7,62 +7,20 @@
    <script src="Script/movies.js" type="text/javascript"></script>
    
     </head>
-    <body onload="init();" >        
-    <br></br>
-    <strong>MOVIES</strong>
-    <a  href="index.html">Home </a>
-    <br></br>
-    <br></br>
-
-    <video id="Video1" controls style="border: 1px solid blue;" height="240" width="100%" title="video element">            
-         HTML5 Video is required for this example
-    </video>
-    
-    <div id="buttonbar" style="display: none;">
-        <button id="restart" title="Restart button">[]</button> 
-        <button id="slower" title="Slower playback button">-</button> 
-        <button id="rew" title="Rewind button" >&lt;&lt;</button>
-        <button id="play" title="Play button">&gt;</button>
-        <button id="fwd" title="Forward button" >&gt;&gt;</button>
-        <button id="faster" title="Faster playback button">+</button>
-        <button id="mute" title="Mute button" ><img alt="Mute button" src="vol2.png" /></button>     
-        <br />
-        <!-- <label>Playback </label> -->
-        <label>Reset playback rate: </label>
-		<button id="normal" title="Reset playback rate button">=</button>    
-        <label>  Volume </label>
-            <button id="volDn"  title="Volume down button">-</button>
-            <button id="volUp"  title="Volume up button">+</button>
-    </div>   
-    <br/>  
-    <div id= "inputField" style="display:none;width:100%" >
-        <label style="width:100%;float:left">Type or paste a video URL: </label>
-		<br></br>
-        <input type="text" id="imageVideo0" style="width: 80%;float:left;"  title="video file input field" value="http://ie.microsoft.com/testdrive/ieblog/2011/nov/pp4_blog_demo.mp4" />        
-        <button id="loadVideo" title="Load video button" style="width: 20%;float:right;" onclick="getVideobyValueId(0)" >Load</button>
-    </div>
-	<br></br>
-    <div title="Error message area" id="errorMsg" style="color:Red;width:100%"></div>  
-	<br></br>
-<?php
-  require 'Script/phputils.php';
-  $dir    = 'images';
-  $files2 = scandir($dir, 0);
-
-  $lengtharr = count($files2);
-  for ($index = 0;$index < $lengtharr;$index++)
-  {
-      $filename =  $files2[$index];
-      if ((trim($filename) != "..") && (trim($filename) != "."))
-      {
-        $fileext = substr($filename,-3);
-        $fileindex= $index+1;
-        $filesize = human_filesize(filesize($dir."/".$filename));
-        $filename_sz = $filesize;
-        MakeDiv5($filename, $fileext, strval($fileindex),$filename_sz);
-        echo "<br>";
-      }
-   }
-?>
+    <body >        
+      <article>
+      <strong>MOVIES</strong>
+   	  <br></br>
+      <a  href="index.html">Home </a>
+   	  <br></br>
+      <div title="Error message area" id="errorMsg" style="color:Red;width:100%"></div>  
+      <div title="Error message area" id="AjaxMessage" style="color:Red;width:100%"></div>  
+	  <br></br>
+      <?php
+        require 'Script/phputils.php';
+        error_reporting(E_ALL); 
+        ShowImagesFiles(); 
+      ?>
+   </article>
  </body>
 </html>
